dc6ae48bcf064b8dc28dac206a043abed79863b6
Andrew Thomson <athomsonguy@gmail.com>
2014-08-05 00:34:49 -0700

    add max-time feature

--- repeat/__init__.py
+++ repeat/__init__.py
@@ -7,12 +7,18 @@ def xxrange(start, stop=None, step=1):
     yield i
     i=i+step
 
-def repeat(callback, period=0, max_tries=None):
+def repeat(callback, period=0, max_tries=None, max_time=None):
+  if max_time is not None:
+    deadline = time() + int(max_time)
+  else:
+    deadline = None
   for i in xxrange(1, max_tries):
     start_time = int(time())
     ret = callback()
     if ret:
       return True
+    if deadline is not None and time() > deadline:
+      return False
     end_time = int(time())
     sleep_time = max(0.0, period - (end_time - start_time))
     sleep(sleep_time)
